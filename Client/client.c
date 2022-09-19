4ef2e9cc8e0e2f67914ddffcd5b4d3f2b6847ddc
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2022-09-19 00:31:24 -0600

    Client for sending a small set of information across

--- Client/client.c
+++ Client/client.c
@@ -1,14 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int readfile(FILE *inputFile, FILE *outputFile);
 
 int requestInfo();
 
 int main(int argc, char *argv[]){
+    int sd;
+    struct sockaddr_in server_address;
+    char filename[20];
+    char buffer[100] = "hello world";
+    int portNumber;
+    char serverIP[29];
+    int rc = 0;
 
-    requestInfo();
+    if (argc < 3) {
+        printf("Usage: %s <server ip> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+
+    portNumber = strtol(argv[1], NULL,10);
+    strcpy(serverIP, argv[1]);
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(portNumber);
+    server_address.sin_addr.s_addr = inet_addr(serverIP);
+
+    if (connect(sd, (struct sockaddr *)&server_address, sizeof(struct sockaddr_in)) < 0) {
+        close(sd);
+        perror("error connecting stream socket");
+        exit(1);
+    }
+    printf("What is the name of the file you'd like to send");
+    scanf("%s", filename);
+    printf("filename to send it '%s'\n", filename);
+    int sizeOfFileName = strlen(filename);
+    int converted_sizeOfFileName = ntohs(sizeOfFileName);
+
+
+
+    rc = write(sd, &converted_sizeOfFileName, sizeof(converted_sizeOfFileName));
+    printf("wrote %d bytes to send the filesize\n", rc);
+    if (rc < 0) {
+        perror("error writing");
+    }
+
+    rc = write(sd, buffer, converted_sizeOfFileName);
+    printf("sent %d bytes\n to send the filename\n", rc);    
+    if(rc < 0){
+        perror("error writing");
+    }
+    return 0;
+    //requestInfo();
 }
 
 int requestInfo()
@@ -22,18 +72,20 @@ int requestInfo()
 
         printf("%s","Please enter in an input file name.\n");
         scanf(" %[^\n]s", iFile);
-        printf("%s","Please enter in an output file name.\n");
-        scanf(" %[^\n]s",oFile);  
-        int rc;
-        char inputFileName[strlen(iFile)];
-        char outputFileName[strlen(oFile)];
-
 
         isDone = strcmp(iFile, "Done");
         if (isDone == 0) {
             printf("Done ~kya");
             return 0;
         }
+
+
+        printf("%s","Please enter in an output file name.\n");
+        scanf(" %[^\n]s",oFile);  
+        int rc;
+        char inputFileName[strlen(iFile)];
+        char outputFileName[strlen(oFile)];
+
 
         memcpy(inputFileName, iFile, strlen(iFile));
         memcpy(outputFileName, oFile, strlen(oFile));
